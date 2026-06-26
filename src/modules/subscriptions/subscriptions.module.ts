7f5674698fd8a915c901df087f0aabe98f633b30
Islam412 <islam.124.hamdy@gmail.com>
2026-06-26 23:28:06 +0300

    desing subscriptions module

--- src/modules/subscriptions/subscriptions.module.ts
+++ src/modules/subscriptions/subscriptions.module.ts
@@ -3,7 +3,8 @@ import { SubscriptionsService } from './subscriptions.service';
 import { SubscriptionsController } from './subscriptions.controller';
 
 @Module({
+  controllers: [SubscriptionsController],
   providers: [SubscriptionsService],
-  controllers: [SubscriptionsController]
+  exports: [SubscriptionsService],
 })
 export class SubscriptionsModule {}
