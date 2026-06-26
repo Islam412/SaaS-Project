0275badeed3472b35fa8251ee3cb95ead7dabbf7
Islam412 <islam.124.hamdy@gmail.com>
2026-06-26 23:54:31 +0300

    create new user

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,12 +7,8 @@ import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { TenantModule } from './modules/tenant/tenant.module';
 import { UsersModule } from './modules/users/users.module';
-import { CustomersModule } from './modules/customers/customers.module';
-import { PlansModule } from './modules/plans/plans.module';
-import { PlansModule } from './modules/plans/plans.module';
 import { SubscriptionsModule } from './modules/subscriptions/subscriptions.module';
 import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
-import { CustomersModule } from './modules/customers/customers.module';
 
 @Module({
   imports: [
@@ -24,8 +20,6 @@ import { CustomersModule } from './modules/customers/customers.module';
     AuthModule,
     TenantModule,
     UsersModule,
-    CustomersModule,
-    PlansModule,
     SubscriptionsModule,
   ],
   controllers: [AppController],
