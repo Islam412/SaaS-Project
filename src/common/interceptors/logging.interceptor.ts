5d64ee974f3e6d6f8ddc6499a1c03eec1fac8829
Islam412 <islam.124.hamdy@gmail.com>
2026-06-26 23:38:13 +0300

    desing logging interceptor

---
+++ src/common/interceptors/logging.interceptor.ts
@@ -0,0 +1,24 @@
+import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+@Injectable()
+export class LoggingInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const now = Date.now();
+    const request = context.switchToHttp().getRequest();
+    const method = request.method;
+    const url = request.url;
+
+    console.log(`📥 ${method} ${url} - Started`);
+
+    return next
+      .handle()
+      .pipe(
+        tap(() => {
+          const duration = Date.now() - now;
+          console.log(`📤 ${method} ${url} - Completed in ${duration}ms`);
+        }),
+      );
+  }
+}
